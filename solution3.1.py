2a4963528d684e0119e8739d84ce1d88ea957207
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2021-08-30 16:13:11 +0300

    Update solution3.1.py

--- solution3.1.py
+++ solution3.1.py
@@ -24,7 +24,7 @@ def isDead(M,F):
     if(b == 0 and ntr):
         #print('dead',M,F)
         return True
-    
+
     while(b >1 and (a % b > 0)):
 
         #print(M,F,'b',b,'a','ceil:',math.ceil((M-F)/F)*F)
@@ -37,14 +37,14 @@ def isDead(M,F):
             a = M
             F = F - (F//M)*M
             length = length + (F//M)
-                
+
         if(M > F):
             b = M % F
         else:
             b = F % M
-        
+
         #print(M,F,'b',b,'a',a,'length:',length)
-        
+
     if(b <= 1):
         #print('false',M,F,'b=',b,'a=',a,'length:',length)
         return False
@@ -52,8 +52,8 @@ def isDead(M,F):
         if( a % b == 0 ):
             #print(M,F)
             return True
-        
-    
+
+
 
 
 def getFibIndex(fn):
@@ -64,12 +64,11 @@ def getFibIndex(fn):
 
 def checkFib(M,F):
     #check if fibonachi
-    if(isFib(M) and isFib(F) and F >1 and M >1):
+    if(isFib(M) and isFib(F)):
         f = getFibIndex(M)
         n = getFibIndex(F)
-        #print('fibs:',f,n,'MF',M,F)
         if(abs(f-n) ==1):
-            #print('iS fib:',max(f,n))
+            print('iS fib:',max(f,n))
             return max(f,n)
     return -1
 
@@ -84,6 +83,29 @@ def isFib(n):
     return n==0 or abs(round(a) - a) < 1.0 / n #n == 0 or abs(round(a) - a) < 1.0 / n
 
 
+#fibonachi number of series starting from A,B >1
+def isTrick(M,F):
+
+    mx = max(M,F)
+    mn = min(M,F)
+    b = 34*min(M,F) - 21*max(M,F)
+    a = (mx - 55*b)/34
+
+    #if(b > 1):
+    if(b > 0 and M>1 and F>1):
+        if(a//1 == a and a > 1):#a is integer
+            #print('found trick',a//1,b)
+            #return True,a//1,b,9
+            return True,round(a//1),b,9
+    #else:
+        #if(b == 1 and a==1):
+            #is fibonachi
+    return False,0,0,0
+
+
+
+
+isFibb = False
 
 
 def recon(M,F):
@@ -91,28 +113,29 @@ def recon(M,F):
     length = 0
     impossible = False
     found = False
+    global isFibb
 
     #i=0
     while( (not impossible) and (not found) ):
-        
+
         #i = i+1
         #if(i > 10**10):
             #print('Performance issues',M,F)
             #print('before: M=',M,'F=',F,'res=',res,length)
          #   impossible == True
-        
+
         if((M ==1 or F ==1) and (F>0 and M >0)  ):
             if(not M == F):
                 if(M > F):
-                    length = length + M - 1 
+                    length = length + M - 1
                 else:
                     length = length + F - 1
             else:
                 length = length+1
             found = True
         else:
-                        
-            #print(M,F)       
+
+            #print(M,F)
             if ( (M < 1 or F < 1) ):#or isDead(M,F) or F==M
                 impossible = True
             else:
@@ -120,18 +143,24 @@ def recon(M,F):
                 if(M > F):
                     res = M - (M//F)*F
                     length = length + M//F
-                    M = res                    
+                    M = res
                 else:
                     res = F - (F//M)*M
                     length = length + F//M
                     F = res
-                
-                    #if(M > 1 and F >1):
-                     #   n = checkFib(M,F)
-                     #   if(not(n==-1)):
-                     #       length =  length + n-3 #length
-                     #       found = True
-                        
+
+                rs = isTrick(M,F)
+                if( rs[0]):
+                    M = rs[1]
+                    F = rs[2]
+                    length = length + rs[3]
+
+                if(False and (M > 5 and F >5)):
+                    n = checkFib(M,F)
+                    if(not(n==-1)):
+                        length =  length + n-2 #length
+                        found = True
+
 
                 #print(M,F,res)
 
@@ -143,7 +172,7 @@ def recon(M,F):
     if(impossible):
         return -1
 
-    
+
 
 
 
@@ -155,19 +184,19 @@ def solution(M,F):
 
     M = int(M)
     F = int(F)
-    
+
     if(M > 10**50 or F>10**50):
         return "impossible"
-    
+
     if(M  < 1 or F < 1):
         return "impossible"
-        
+
     if(M==1 and F ==1):
         return ("%.0f" % (0))
-    
-    if( (M//1) < M or (F//1) < F): 
+
+    if( (M//1) < M or (F//1) < F):
         return "impossible"
-    
+
 
     minLen = 0
 
@@ -178,5 +207,5 @@ def solution(M,F):
     if(minLen == -1):
         return "impossible"
     #else: return ("%.0f" % (minLen))
-    else: return ("%.0f" % (int(minLen//1)))
+    else: return ("%.0f" % (int(minLen)))
     #else: return ("%.0g" % (minLen))
