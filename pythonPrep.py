405db1bab97c34669ca9eeb949fb087d4007ef00
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2021-10-31 11:36:59 +0200

    Update pythonPrep.py

--- pythonPrep.py
+++ pythonPrep.py
@@ -1,1 +1,26 @@
-def
+
+#loops example
+def loops(start,finish)
+  i = 1
+    while i < 6:
+      print(i)
+      i += 1
+  
+  
+  fruits = ["apple", "banana", "cherry"]
+  
+  for x in fruits:
+    print(x)
+
+
+#Conditions examples:
+def conditions()
+
+  a = 33
+  b = 200
+  if b > a:
+    print("b is greater than a")
+
+
+
+      
